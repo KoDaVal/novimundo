6990fb7c62570ea9e1e48dfb35871659e427b8df
KoDaVal <99213984+KoDaVal@users.noreply.github.com>
2026-01-11 22:33:59 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx' // Importa tu archivo gigante
 import './index.css' // Importa los estilos de Tailwind
+import { HelmetProvider } from 'react-helmet-async';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+
 )
