cb0842116708ad07324b47df98c92766e826b625
KoDaVal <99213984+KoDaVal@users.noreply.github.com>
2026-01-11 23:10:22 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx' // Importa tu archivo gigante
-import './index.css' // Importa los estilos de Tailwind
+import App from './App.jsx'
+import './index.css'
 import { HelmetProvider } from 'react-helmet-async';
+
+// 1. IMPORTANTE: Creamos esta variable vacía para evitar el error de "undefined"
+const helmetContext = {};
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* 2. Envolvemos la App y le pasamos el contexto */}
+    <HelmetProvider context={helmetContext}>
+      <App />
+    </HelmetProvider>
   </React.StrictMode>,
-
 )
